a04b4846ed3544b50a9c69632ab08010e1a62f93
ronniebelenario25-sys <ronniebelenario25@gmail.com>
2026-09-12 22:02:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import plotly.express as px
 import streamlit as st
 from supabase import create_client
 
-# Direct Supabase Credentials (Bypassing Streamlit Secrets)
-https://rwcvjhpjnelefauoajkq.supabase.co
+# Direct Supabase Credentials
+SUPABASE_URL = "https://rwcvjhpjnelefauoajkq.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3Y3ZqaHBqbmVsZWZldW9hamtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMTM3ODcsImV4cCI6MjEwNDc4OTc4N30.8XkTZys3WGP5JiJIo6-MlQeCmtlMJJS0EwRvFcIj-8I"
 
 
